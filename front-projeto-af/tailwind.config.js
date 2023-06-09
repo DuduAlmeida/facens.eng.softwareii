521be2aa9a0e53f6ae292fd7cc912fc73284078a
Eduardo Almeida <contato.eduardopex@hotmail.com>
2023-06-09 11:54:58 -0300

    feat: started to create front code.

---
+++ front-projeto-af/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // JavaScript and TypeScript files
+    "./pages/**/*.{js,jsx,ts,tsx}", // Next.js pages
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
